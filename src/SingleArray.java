b2276ca6b2ee3e08fdd8636859449c6114be0769
Vemula Mohith Venkata Sai <mohithvemula1@gmail.com>
2022-07-19 14:36:51 +0530

    Arrays Programme

---
+++ src/SingleArray.java
@@ -0,0 +1,13 @@
+
+public class SingleArray {
+
+	public static void main(String[] args) {
+		int a[] = { 10, 20, 30, 40, 50 ,60 , 70 };
+		
+		for (int i = 0; i <7 ; i++) {
+			System.out.println("Elements of array a: " + a[i]);
+		}
+
+	}
+
+}
